c94ce2ac7ce1eb3e4abd551f6be8774c95ea1292
simplyarav <simplyaravv@gmail.com>
2026-04-09 22:41:56 +0530

    fix missing reset method

--- env/environment.py
+++ env/environment.py
@@ -8,19 +8,19 @@ class TrafficEnv:
         self.task = task
         self.reset()
 
-def reset(self):
-    if self.task == "easy":
-        self.queues = {"N": 2, "S": 2, "E": 2, "W": 2}
+    def reset(self):
+        if self.task == "easy":
+            self.queues = {"N": 2, "S": 2, "E": 2, "W": 2}
 
-    elif self.task == "medium":
-        self.queues = {"N": 5, "S": 5, "E": 3, "W": 3}
+        elif self.task == "medium":
+            self.queues = {"N": 5, "S": 5, "E": 3, "W": 3}
 
-    elif self.task == "hard":
-        self.queues = {"N": 8, "S": 8, "E": 6, "W": 6}
+        elif self.task == "hard":
+            self.queues = {"N": 8, "S": 8, "E": 6, "W": 6}
 
-    self.time = 0
-    self.emergency = None
-    return self.state()
+        self.time = 0
+        self.emergency = None
+        return self.state()
 
     def step(self, action):
         self.state_data["signal"] = action.signal
